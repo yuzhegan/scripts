7c0fc8d47a76017a7bd77a300190b31d8c9aeddd
yuzhegan <boflyyang@163.com>
2021-11-27 22:02:03 +0800

    light up down

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-<<<<<<< HEAD
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 xmodmap ~/.config/.Xmodmap &
-=======
 #xmodmap ~/.config/.Xmodmap
 #xrandr -s 2560x1440
-xrandr --output eDP-1 --scale 0.7x0.7
+#xrandr --output eDP-1 --scale 0.7x0.7
 /bin/bash ~/scripts/dwm-status.sh &
 #/bin/bash ~/scripts/wp-autochange.sh &
->>>>>>> 528b474b6b2b67e884e00610521cbeec9b243313
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
@@ -19,13 +16,10 @@ fcitx5
 nm-applet &
 xfce4-power-manager &
 #xfce4-volumed-pulse &
-<<<<<<< HEAD
 /bin/bash ~/scripts/run-mailsync.sh &
 /bin/bash ~/scripts/tmux.sh &
 #~/scripts/autostart_wait.sh &
-=======
 #/bin/bash ~/scripts/run-mailsync.sh &
 #~/scripts/autostart_wait.sh &
 xmodmap ~/.config/.Xmodmap &
 
->>>>>>> 528b474b6b2b67e884e00610521cbeec9b243313
---
+++ light-up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+light -A 5
+#/usr/bin/amixer -qM set Master 5%- umute
+##pactl set-sink-volume @DEFAULT_SINK@ -5%
+#bash ~/scripts/dwm-status-refresh.sh
